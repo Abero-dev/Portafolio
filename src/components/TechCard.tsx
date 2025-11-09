de97cd0958d623d3e0bf8c9da1233c514d417028
Abero-dev <abel04.mata@gmail.com>
2025-11-09 02:10:27 -0500

    Se añadió la sección Tecnologías

---
+++ src/components/TechCard.tsx
@@ -0,0 +1,25 @@
+interface TechCardProps {
+    title: string,
+    icons: string[],
+}
+
+function TechCard({ title, icons }: TechCardProps) {
+    return (
+        <div className="flex flex-col gap-y-8 bg-[#00120c] px-8 py-4 border-2 border-green-950 rounded-xl hover:border-green-700 transition-colors duration-300">
+            <p className="text-2xl text-green-500 text-center font-bold">{title}</p>
+            <hr className="border-2 border-green-700" />
+            <div className="grid grid-cols-3 gap-8 ">
+                {icons.map(i =>
+                    <img
+                        key={i}
+                        src={`icons/${i}.svg`}
+                        alt={i}
+                        className="w-20 h-20"
+                    />)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default TechCard
--- src/sections/Tecnologías.tsx
+++ src/sections/Tecnologías.tsx
@@ -1,6 +1,28 @@
+import TechCard from "../components/TechCard"
+
 function Tecnologías() {
     return (
-        <section className="min-h-screen"></section>
+        <section id="Tecnologías" className="min-h-screen flex flex-col justify-center items-center gap-y-10">
+            <p className="text-white text-2xl">
+                A continuación se muestran las tecnologías con las que he interactuado recientemente:
+            </p>
+            <div className="flex justify-center gap-x-15">
+                <TechCard
+                    title="Frontend"
+                    icons={["HTML5", "CSS3", "TailwindCSS", "JavaScript", "TypeScript", "React", "Redux Toolkit", "Tanstack Query", "Zustand"]}
+                />
+                <div className="flex flex-col gap-y-5">
+                    <TechCard
+                        title="Backend"
+                        icons={["Nest.js", "PostgresSQL"]}
+                    />
+                    <TechCard
+                        title="Herramientas"
+                        icons={["Git", "Docker", "Playwright"]}
+                    />
+                </div>
+            </div>
+        </section>
     )
 }
 
